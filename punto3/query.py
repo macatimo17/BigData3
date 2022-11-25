8009a556564b433355a4c1898aceb7c1b1a1e776
Maria Camila Tibasosa Molina <90457433+macatimo17@users.noreply.github.com>
2022-11-25 13:11:12 -0600

    Update query.py

--- punto3/query.py
+++ punto3/query.py
@@ -5,7 +5,6 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 table = dynamodb.Table('Animals')
 
-print("Checkeando peces")
 
 response = table.query(
     KeyConditionExpression=Key('indice').eq("Fish")
