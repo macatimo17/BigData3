708657726fa0ec0f8051482d73381a00cbbc1a89
Maria Camila Tibasosa Molina <90457433+macatimo17@users.noreply.github.com>
2022-11-25 13:10:55 -0600

    Update animals_database.py

--- punto3/animals_database.py
+++ punto3/animals_database.py
@@ -3,7 +3,7 @@ import boto3
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
 table = dynamodb.create_table(
-    TableName='Animals2',
+    TableName='animales',
     KeySchema=[
         {
             'AttributeName': 'indice',
